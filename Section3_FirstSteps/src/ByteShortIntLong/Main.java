1828852ff14e93567935f15ed4e33dbb9f3e2851
rominalodolo <lodoloromina@gmail.com>
2022-04-20 01:04:45 +0200

    update

---
+++ Section3_FirstSteps/src/ByteShortIntLong/Main.java
@@ -0,0 +1,4 @@
+package ByteShortIntLong;
+
+public class Main {
+}
--- Section3_FirstSteps/src/HelloWorld.java
+++ Section3_FirstSteps/src/HelloWorld.java
@@ -2,5 +2,22 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Hi, Romina");
+
+        // Variables
+        int myFirstNumber = 5;
+        System.out.println(myFirstNumber);
+
+        int mySecondNumber = ( 14 + 2 ) + ( 8 * 3 );
+        System.out.println(mySecondNumber);
+
+
+        // Expressions
+        int myThirdNumber = myFirstNumber + mySecondNumber * 9;
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+        System.out.println(myTotal);
+
+        int myLastOne = 1000 - myTotal;
+        System.out.println(myLastOne);
+
     }
 }
