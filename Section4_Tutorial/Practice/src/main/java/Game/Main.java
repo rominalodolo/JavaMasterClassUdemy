2186020192b1dc0d6f647094df478dfdc67b850f
rominalodolo <lodoloromina@gmail.com>
2022-04-20 14:03:25 +0200

    new

---
+++ Section4_Tutorial/Practice/src/main/java/Game/Main.java
@@ -0,0 +1,10 @@
+package Game;
+
+public class Main {
+    public static void main(String[] args) {
+        boolean gameOver = true;
+        int score = 800;
+        int levelCompleted = 5;
+        int bonus = 100;
+    }
+}
