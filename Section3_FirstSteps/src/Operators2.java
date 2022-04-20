6309d052bc390d92a03962af8b85481c2c16d52a
rominalodolo <lodoloromina@gmail.com>
2022-04-20 02:27:11 +0200

    update

--- Section3_FirstSteps/src/Operators2.java
+++ Section3_FirstSteps/src/Operators2.java
@@ -16,5 +16,28 @@ public class Operators2 {
 
         result = result % 3;    // the remainder of (4 % 3) = 1 (3 goes into 4 once)
         System.out.println("4 % 3 = " + result);
+
+        // result = result + 1; Postfix Increment
+        result++; // 1 + 1 = 2
+        System.out.println("1 + 1 = " + result);
+
+        result--;  // 2 - 1 = 1; Decrement Operator subtracts 1 from the operand.
+        System.out.println("2 - 1 = " + result);
+
+        // result = result +2;
+        result += 2;  // 1 + 2 = 3
+        System.out.println("1 + 2 = " + result);
+
+        // result = result * 10;
+        result *= 10; // 3 * 10 = 30
+        System.out.println("3 * 10 = " + result);
+
+        // result = result / 3
+        result /= 3;  // 30 / 3 = 10
+        System.out.println("30 / 3 = " + result);
+
+        // result = result - 2
+        result -= 2;  // 10 - 2 = 8
+        System.out.println("10 - 2 = " + result);
     }
 }
