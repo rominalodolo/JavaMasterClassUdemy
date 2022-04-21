0ba231977c98a65f5df2398dd2e4bb8f12bc7a29
rominalodolo <lodoloromina@gmail.com>
2022-04-21 11:05:29 +0200

    update

--- Section4_Tutorial/Exercise1/src/SpeedConverter.java
+++ Section4_Tutorial/Exercise1/src/SpeedConverter.java
@@ -1,7 +1,8 @@
 public class SpeedConverter {
 
     public static long toMilesPerHour(double kilometersPerHour){
-
+        if(kilometersPerHour < 0 ){
+            return -1;
+        }
     }
-
 }
