fba9a239a2d099a404391c7987f800cde57153a1
rominalodolo <lodoloromina@gmail.com>
2022-04-21 14:29:21 +0200

    update

--- Section4_Tutorial/Exercise2/src/MegaBytesConverter.java
+++ Section4_Tutorial/Exercise2/src/MegaBytesConverter.java
@@ -1,2 +1,24 @@
 public class MegaBytesConverter {
+
+    int kilobytes = XX;
+    int megabytes = YY;
+    int remainder = ZZ;
+
+    public static void printMegaBytesAndKiloBytes {
+        System.out.println(printMegaBytesAndKiloBytes(2500));
+        System.out.println(printMegaBytesAndKiloBytes(-1024));
+        System.out.println(printMegaBytesAndKiloBytes(5000));
+    }
+
+
 }
+//
+//XX = original value kiloBytes
+//YY = calculated megabytes
+//ZZ = calculated remaining kilobytes
+
+// kiloBytes is less than 0 "Invalid Value"
+// YY < 0 return ("Invalid Value")
+// print format "XX KB = YY MB and ZZ KB"
+// remainder operator
+// 1 MB = 1024 KB
