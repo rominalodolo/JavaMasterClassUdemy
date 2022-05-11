df6f52bf45abe0addc64926f9eac058b53320ad0
rominalodolo <lodoloromina@gmail.com>
2022-05-11 15:39:29 +0200

    update

--- Section4_Tutorial/Exercise1/src/SpeedConverter.java
+++ Section4_Tutorial/Exercise1/src/SpeedConverter.java
@@ -4,8 +4,9 @@ public class SpeedConverter {
         if(kilometersPerHour < 0 ){
             return -1;
         }
-        return Math.round(kilometersPerHour / 1.609);
+        return Math.round(kilometersPerHour * 1.609);
     }
+
     public static void printConversion(double kilometersPerHour){
 
         if(kilometersPerHour < 0){
