d81cafdda6b91eebd1643027d57789797dbc4979
rominalodolo <lodoloromina@gmail.com>
2022-05-12 14:47:34 +0200

    update

--- Section4_Tutorial/Exercise1/src/Main.java
+++ Section4_Tutorial/Exercise1/src/Main.java
@@ -1,16 +1,16 @@
-
-
-public class Main {
-    public static void main(String[] args) {
-
-        toMilesPerHour(1.5);
-        toMilesPerHour(10.25);
-        toMilesPerHour(-5.6);
-        toMilesPerHour(25.42);
-        toMilesPerHour(75.114);
-
-        long miles = SpeedConverter.toMilesPerHour(12.9);
-        System.out.println("Miles = " + miles);
-
-    }
-}
+//
+//
+//public class Main {
+//    public static void main(String[] args) {
+//
+//        toMilesPerHour(1.5);
+//        toMilesPerHour(10.25);
+//        toMilesPerHour(-5.6);
+//        toMilesPerHour(25.42);
+//        toMilesPerHour(75.114);
+//
+//        long miles = SpeedConverter.toMilesPerHour(12.9);
+//        System.out.println("Miles = " + miles);
+//
+//    }
+//}
