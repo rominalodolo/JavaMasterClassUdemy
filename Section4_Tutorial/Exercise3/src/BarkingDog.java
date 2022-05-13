b7ce79ac24a4de42a7a52a48c549b8a94d562bbf
rominalodolo <lodoloromina@gmail.com>
2022-05-13 13:07:48 +0200

    UPDATE

--- Section4_Tutorial/Exercise3/src/BarkingDog.java
+++ Section4_Tutorial/Exercise3/src/BarkingDog.java
@@ -2,27 +2,26 @@ public class BarkingDog {
 
     public static boolean shouldWakeUp (boolean barking, int hourOfDay) {
         if (hourOfDay < 0 | hourOfDay > 23) { // arguments are out of range i.e. invalid
+            return false;
 
+        } else { // valid argument
             if (barking) { // dog is barking, so check the time
-                if (hourOfDay >= 22 & hourOfDay <= 8) {  // if the time is in tht sleep hours then return true
+                if ((hourOfDay > 22) || (hourOfDay < 8)) {  // if the time is in tht sleep hours then return true
                     return true; // must wake up
                 }else {
                     return false;
                 }
-            } else { // no need to wake
+            }else { // no need to wake
                 return false;
             }
-
-        } else { // valid argument
-            return true;
         }
     }
 
     public static void main(String[] args) {
-        shouldWakeUp(true,1);
-        shouldWakeUp (false, 2);
-        shouldWakeUp (true, 8);
-        shouldWakeUp (true, -1);
+        System.out.println(shouldWakeUp(true,1));
+        System.out.println(shouldWakeUp(false, 2));
+        System.out.println(shouldWakeUp(true, 8));
+        System.out.println(shouldWakeUp(true, -1));
     }
 }
 
