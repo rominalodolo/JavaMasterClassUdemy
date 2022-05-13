402148afdb84ae614fdba1f67c5bba8801d811f4
rominalodolo <lodoloromina@gmail.com>
2022-05-13 13:08:18 +0200

    UPDATE

--- Section4_Tutorial/Exercise5/src/DecimalComparator.java
+++ Section4_Tutorial/Exercise5/src/DecimalComparator.java
@@ -1,2 +1,17 @@
 public class DecimalComparator {
+
+    public static boolean areEqualByThreeDecimalPlaces(double paper, double pencil){
+        if ((int) (paper * 1000) == (int) (pencil * 1000)) {
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(areEqualByThreeDecimalPlaces(-3.1756, -3.175));
+        System.out.println(areEqualByThreeDecimalPlaces(3.175, 3.176));
+        System.out.println(areEqualByThreeDecimalPlaces(3.0, 3.0));
+        System.out.println(areEqualByThreeDecimalPlaces(-3.123, 3.123));
+    }
 }
