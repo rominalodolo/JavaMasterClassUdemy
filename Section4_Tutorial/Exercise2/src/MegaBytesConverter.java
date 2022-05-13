eceddfb87e74729b6c4245a5364b6b020f5a6254
rominalodolo <lodoloromina@gmail.com>
2022-05-13 10:58:16 +0200

    update

--- Section4_Tutorial/Exercise2/src/MegaBytesConverter.java
+++ Section4_Tutorial/Exercise2/src/MegaBytesConverter.java
@@ -1,13 +1,17 @@
 public class MegaBytesConverter {
 
-
     public static void printMegaBytesAndKiloBytes(int kiloBytes){
-        System.out.println(kiloBytes + " KB = " + (kiloBytes / 1024) + " MB and " + (kiloBytes % 1024) + " KB.");
+        if (kiloBytes < 0 ){
+            System.out.println("Invalid Value");
+        } else {
+            System.out.println(kiloBytes + " KB = " + (kiloBytes / 1024) + " MB and " + (kiloBytes % 1024) + " KB.");
+        }
     }
-
 
     public static void main(String[] args) {
         printMegaBytesAndKiloBytes(2500);
+        printMegaBytesAndKiloBytes(-1024);
+        printMegaBytesAndKiloBytes(5000);
     }
 }
 
