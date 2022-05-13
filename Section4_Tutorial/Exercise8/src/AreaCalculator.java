3e7e0437ca3d609caf461ce3448bf3c30306993d
rominalodolo <lodoloromina@gmail.com>
2022-05-13 13:46:44 +0200

    update

--- Section4_Tutorial/Exercise8/src/AreaCalculator.java
+++ Section4_Tutorial/Exercise8/src/AreaCalculator.java
@@ -1,10 +1,24 @@
+import static java.lang.Math.PI;
+
 public class AreaCalculator {
     public static double area (double radius){
+        if (radius * radius * PI){
 
+        }
+        return -1;
     }
 
-    public static double area (double radius){
+    public static double area (double x, double y){
+        if (x * y){
 
+        }
+        return -1;
     }
 
+    public static void main(String[] args) {
+        System.out.println(area(5.0));
+        System.out.println(area(-1));
+        System.out.println(area(5.0, 4.0));
+        System.out.println(area(-1.0, 4.0));
+    }
 }
