5af7d02079616b7fa930da40c01055aa34e41d50
rominalodolo <lodoloromina@gmail.com>
2022-05-13 13:20:16 +0200

    update

--- Section4_Tutorial/Exercise7/src/TeenNumberChecker.java
+++ Section4_Tutorial/Exercise7/src/TeenNumberChecker.java
@@ -1,2 +1,20 @@
 public class TeenNumberChecker {
+    public static boolean hasTeen (int age1, int age2, int age3){
+        
+    }
+
+    public static boolean isTeen (int age){
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(hasTeen(9, 99, 19));
+        System.out.println(hasTeen(23, 15, 42));
+        System.out.println(hasTeen(22, 23, 34));
+
+        System.out.println("\n");
+
+        System.out.println(isTeen(9));
+        System.out.println(isTeen(9));
+    }
 }
